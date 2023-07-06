f9f44b92d4a118d78c4d93ca253c82f5e4ef321d
durga1808 <durgalakshmi3713@gmail.com>
2023-07-06 09:50:58 +0530

    updated

---
+++ varchar/src/main/java/com/chars/app/App.java
@@ -0,0 +1,13 @@
+package com.chars.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
