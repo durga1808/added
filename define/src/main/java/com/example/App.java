2523d6ecf3bb9f50f0adb7827509ebda7e0b12f5
durga1808 <durgalskshmi3713@gmail.com>
2023-08-08 09:40:13 +0530

    inserted

---
+++ define/src/main/java/com/example/App.java
@@ -0,0 +1,16 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args )
+    {
+       // System.out.println( "Hello World!" );
+       Main main =new Main(int id,String name);
+        main.id;
+        main.name;
+    }
+}
---
+++ define/src/main/java/com/example/Main.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Main {
+    
+    public int id=101;
+   public  String  name="Durga";
+    
+}
