d1855c975a87d0664ba3503803403b139a9a142e
durga1808 <137372215+durga1808@users.noreply.github.com>
2023-08-08 10:56:55 +0530

    Update App.java

--- define/src/main/java/com/example/App.java
+++ define/src/main/java/com/example/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main(String[] args )
     {
-       // System.out.println( "Hello World!" );
+       // System.out.println( "Durga.....!" );
        Main main =new Main(int id,String name);
         main.id;
         main.name;
