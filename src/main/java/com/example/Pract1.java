3e6e48be8ab97a0a6622b8f0b020eef20cac8b0f
umamuthu94 <umamuthu1994@gmail.com>
2023-08-11 14:17:35 +0530

    how to work maven life cycles

---
+++ src/main/java/com/example/Pract1.java
@@ -0,0 +1,15 @@
+package com.example;
+
+//import com.ex
+
+/**
+ * Hello world!
+ *
+ */
+public class Pract1 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hi" );
+    }
+}
